7aba866f53cd6a0158b6c4c796e1390dd40e118c
himeshiddamalgoda <himeshiddamalgoda@gmail.com>
2022-01-03 19:24:03 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD91hTuhp5yxB8xzYYcP9eliTazEQwrN2k",
+    authDomain: "whatsapp-66b6a.firebaseapp.com",
+    projectId: "whatsapp-66b6a",
+    storageBucket: "whatsapp-66b6a.appspot.com",
+    messagingSenderId: "615280114806",
+    appId: "1:615280114806:web:a481c6f0bb093e7ea87357"
+  };
